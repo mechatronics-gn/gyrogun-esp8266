4f443ef062fd405fcaef5f415408747482d9a973
Janghyub Seo <jhyub06@gmail.com>
2023-05-25 21:46:28 +0900

    initial commit

---
+++ gyrogun-esp8266.ino
@@ -0,0 +1,38 @@
+#include<ESP8266WiFi.h>
+#include "secrets.h"
+
+WiFiClient client;
+IPAddress host HOST_IP;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(MECHANET_SSID, MECHANET_PW);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+int cnt = 0;
+uint8_t buffer[16] = {};
+
+void loop() {  
+  if(!client.connected()) {
+    Serial.write(0);
+    cnt = 0;
+    client.connect(host, HOST_PORT);
+    delay(1000);
+  } else {
+    Serial.write(1);
+  }
+  
+  if(Serial.available()) {
+    if (cnt < 16) buffer[cnt++] = Serial.read();
+    if (cnt == 16) {
+      client.write(buffer, 16);
+      cnt = 0;
+    }
+  }
+  delayMicroseconds(250);
+}
